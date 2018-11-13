fbb09092398cc36df76119631394096dbad97607
Rus <realqwazwak@gmail.com>
2018-11-13 09:51:28 -0800

    started

---
+++ Program-04/BinaryTree.cpp
@@ -0,0 +1,12 @@
+/*********************************************************************************
+ *     Title:   BinaryTree.cpp                                                   *
+ *     Author:  PUT_YOUR_NAME_HERE                                               *
+ *     Date:    March 29, 2018                                                   *
+ *     Purpose: This is the implementation file for the BinaryTree class, which  *
+ *              is an implementation of a Binary Search Tree.  Each Tree Node    *
+ *              stores a customer name (string) and the number of Krabby Patties *
+ *              the customer ate.                                                *
+ *********************************************************************************/
+#include <iostream>
+#include "BinaryTree.h"
+using namespace std;
